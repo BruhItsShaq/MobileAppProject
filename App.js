11374a4c2f6f1764125999ff37a0e1fde74f7571
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2023-02-20 14:14:47 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import LoginScreen from './components/login';
+
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <LoginScreen />
+      </View>
+      // <FlatListDemo />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
