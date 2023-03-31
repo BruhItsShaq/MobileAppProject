2ce29d50b34c58de2cd069055298a01c7f14a12b
SHAQS-LAPTOP\shaqe <shaqeil567@outlook.com>
2023-03-31 01:23:48 +0100

    Navigation set up for block screen within contacts

--- App.js
+++ App.js
@@ -6,31 +6,41 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './components/homeScreen';
 import ProfileScreen from './components/profileScreen';
 import ContactsScreen from './components/contactsScreen';
+import BlockedScreen from './components/blockedScreen';
 import LoginScreen from './components/login';
 import SignUp from './components/signUp';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+function ContactsStack() {
+  return (
+    <Stack.Navigator initialRouteName="Contacts">
+      <Stack.Screen name="Contacts" component={ContactsScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Blocked" component={BlockedScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
+
 function MainTabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       <Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="Contacts" component={ContactsScreen} options={{ headerShown: false }} />
+      <Tab.Screen name="Contacts" component={ContactsStack} options={{ headerShown: false }} />
     </Tab.Navigator>
   );
 }
 
-export default class App extends Component{
-  render(){
+export default class App extends Component {
+  render() {
     return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName='Login'>
-           <Stack.Screen name="Login" component={LoginScreen}/>
-           <Stack.Screen name="SignUp" component={SignUp} />
-           <Stack.Screen name="Home" component={MainTabs} />
-         </Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Home" component={MainTabs} />
+        </Stack.Navigator>
       </NavigationContainer>
     )
   }
